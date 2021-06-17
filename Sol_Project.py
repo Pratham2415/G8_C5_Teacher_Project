98cf0db25d0dd9f657a235e016acc551074929f0
Pratham2415 <84369631+Pratham2415@users.noreply.github.com>
2021-06-17 10:08:40 +0530

    Add files via upload

---
+++ Sol_Project.py
@@ -0,0 +1,17 @@
+n1 = int(input("Enter number 1: "))
+n2 = int(input("Enter number 2: "))
+
+
+def addition(n1,n2):
+    print(n1+n2)
+def subtraction(n1,n2):
+    print(abs(n1-n2))
+def multiplication(n1,n2):
+    print(n1*n2)
+def division(n1,n2):
+    print(n1//n2)
+    
+addition(n1,n2)
+subtraction(n1,n2)
+multiplication(n1,n2)
+division(n1,n2)
